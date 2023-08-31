8930b0cf58e0a7bbb3fb98d0051c22843c306541
Hien Vu Quang (hienvq) <hienvq@curama.vn>
2023-08-31 20:36:46 +0700

    update

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: `http://localhost:3004`,
+  baseURL: `http://18.143.129.2/api-quang`,
 });
 
 axiosClient.interceptors.response.use((reponse) => {
